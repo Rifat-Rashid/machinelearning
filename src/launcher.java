4295c0573b2bbdd6101875bd209c598f75945988
hengj <hengjuat@gmail.com>
2017-08-16 22:40:37 -0700

    ff

--- src/launcher.java
+++ src/launcher.java
@@ -1,8 +1,18 @@
+import java.lang.reflect.Array;
+import java.util.List;
+
 /**
  * Created by DevWork on 8/16/17.
  */
 public class launcher {
+
+
+    int[] a = {1, 2, };
+
     public static void main(String[] args){
 
+
     }
+
+
 }
