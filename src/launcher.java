091f2da022c05158879d58cc492711665aba0297
DevWork <s-RRashid@lwsd.org>
2017-08-16 23:02:23 -0700

    added graphing capabilities

--- src/launcher.java
+++ src/launcher.java
@@ -14,28 +14,18 @@ import java.util.Random;
  */
 public class launcher {
 
-    //
-    int[] a = {1, 2,};
-
     public static void main(String[] args) {
-        // Create a chart:
         Chart2D chart = new Chart2D();
-        // Create an ITrace:
         ITrace2D trace = new Trace2DSimple();
-        // Add the trace to the chart. This has to be done before adding points (deadlock prevention):
         chart.addTrace(trace);
-        // Add all points, as it is static:
         Random random = new Random();
         for (int i = 100; i >= 0; i--) {
             trace.addPoint(i, random.nextDouble() * 10.0 + i);
         }
-        // Make it visible:
-        // Create a frame.
+
         JFrame frame = new JFrame("MinimalStaticChart");
-        // add the chart to the frame:
         frame.getContentPane().add(chart);
         frame.setSize(400, 300);
-        // Enable the termination button [cross on the upper right edge]:
         frame.addWindowListener(
                 new WindowAdapter() {
                     public void windowClosing(WindowEvent e) {
